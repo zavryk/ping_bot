960bf800926c9ae1e2c625ab0b120f964c46124d
zavryk <zavryk@gmail.com>
2023-11-17 15:06:36 +0200

    upd

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import logging
 import os
 import asyncio
-import datetime
-import httpx
 import socket
 
-from aiogram import Bot, types
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ParseMode
 from aiogram.utils import executor
-from ping3 import ping
-
 
 API_TOKEN = os.getenv('ACCESS_TOKEN')
 MONITORED_IP = os.getenv('NODE')
 YOUR_CHAT_ID = os.getenv('YOUR_CHAT_ID')
-
-
 
 
 logging.basicConfig(level=logging.INFO)
@@ -24,59 +18,42 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-print(MONITORED_IP)
-
 def ping(host, port, timeout=10):
+    try:
+        socket.create_connection((MONITORED_IP, port), timeout=timeout)
+        return True
+    except (socket.timeout, socket.error):
+        return False
+
+
+result = ping(MONITORED_IP, 53131)
+print(result)
+async def check_ip(host, port, timeout=10):
     try:
         socket.create_connection((host, port), timeout=timeout)
         return True
     except (socket.timeout, socket.error):
         return False
 
-# Example
-result = ping(MONITORED_IP,53131)
-print(result)
-
-
-# timeout = 5
-# start = datetime.datetime.now()
-# while (datetime.datetime.now() - start).total_seconds() < timeout:
-#     otvet = ping(MONITORED_IP, timeout=timeout, ttl=20)
-#     if otvet: # завершаем в случае успеха
-#
-#             break
-# print(otvet)
-
-async def check_ip(ip):
-    timeout = 10
-
-    try:
-        ping(MONITORED_IP, 53131)
-        return True
-    except socket.error as e:
-        logging.error(f"Error while checking IP {ip}: {e}")
-
-        return False
-
-
-
-
 
 async def monitor_ip():
-    prev_status = await check_ip(MONITORED_IP)
+    prev_status = await check_ip(MONITORED_IP, 53131)
     while True:
-        current_status = await check_ip(MONITORED_IP)
+        current_status = await check_ip(MONITORED_IP, 53131)
         if current_status != prev_status:
-            await notify_status_change(current_status)
+            await notify_status_change(int(current_status))
             prev_status = current_status
-        # Wait for 30 seconds before the next check
-        await asyncio.sleep(60)
+        # Wait for 60 seconds before the next check
+        await asyncio.sleep(5)
 
 
 @dp.message_handler(commands=['start'])
-async def notify_status_change(is_up: bool):
+async def notify_status_change(status: int):
+    is_up = bool(status)  # Convert to boolean
     status_message = f"IP {MONITORED_IP} {'доступний' if is_up else 'недоступний'}"
     await bot.send_message(chat_id=YOUR_CHAT_ID, text=status_message, parse_mode=ParseMode.MARKDOWN)
+
+
 
 if __name__ == '__main__':
     try:
