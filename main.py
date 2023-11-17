69206602c43b39246a7f0385c59dc1ddc86a30ff
zavryk <zavryk@gmail.com>
2023-11-17 18:17:35 +0200

    upd

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import random
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import ParseMode
+from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 
 API_TOKEN = os.getenv('ACCESS_TOKEN')
 MONITORED_IP = os.getenv('NODE')
 YOUR_CHAT_ID = os.getenv('YOUR_CHAT_ID')
-
 
 response_down = [
     "🔴 Наробився 💽",
@@ -35,11 +34,11 @@ response_up = [
     "🟩 Сьогодні буду работать до ночі 💪"
 ]
 
-
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
 
 def ping(host, port, timeout=10):
     try:
@@ -51,6 +50,8 @@ def ping(host, port, timeout=10):
 
 result = ping(MONITORED_IP, 53131)
 print(result)
+
+
 async def check_ip(host, port, timeout=10):
     try:
         socket.create_connection((host, port), timeout=timeout)
@@ -71,21 +72,37 @@ async def monitor_ip():
 
 
 @dp.message_handler(commands=['start'])
-async def notify_status_change(status: int):
-    is_up = bool(status)  # Convert to boolean
+async def notify_status_change(message):
+    is_up = bool(int(message.get_args()))
     status_message = random.sample(response_up, 1)[0] if is_up else random.sample(response_down, 1)[0]
-    await bot.send_message(chat_id=YOUR_CHAT_ID, text=str(status_message), parse_mode=ParseMode.MARKDOWN)
+    await bot.send_message(chat_id=message.chat.id, text=str(status_message), parse_mode=ParseMode.MARKDOWN)
 
-    @dp.message_handler(commands=['status'])
-    async def check_current_status(message: types.Message):
-        current_status = await check_ip(MONITORED_IP, 53131)
-        is_up = bool(current_status)
-        status_message = random.sample(response_up, 1)[0] if is_up else random.sample(response_down, 1)[0]
-        await bot.send_message(chat_id=message.chat.id, text=str(status_message), parse_mode=ParseMode.MARKDOWN)
 
-    @dp.message_handler(lambda message: message.text == 'А щас?')
-    async def call_status_command(message: types.Message):
-        await check_current_status(message)
+@dp.message_handler(commands=['status'])
+async def check_current_status(message: types.Message):
+    current_status = await check_ip(MONITORED_IP, 53131)
+    is_up = bool(current_status)
+    status_message = random.sample(response_up, 1)[0] if is_up else random.sample(response_down, 1)[0]
+    await bot.send_message(chat_id=message.chat.id, text=str(status_message), parse_mode=ParseMode.MARKDOWN)
+
+
+@dp.message_handler(lambda message: message.text == 'А щас?')
+async def call_status_command(message: types.Message):
+    await check_current_status(message)
+
+
+@dp.inline_handler(lambda query: True)
+async def inline_status_query(inline_query: types.InlineQuery):
+    current_status = await check_ip(MONITORED_IP, 53131)
+    is_up = bool(current_status)
+    status_message = random.sample(response_up, 1)[0] if is_up else random.sample(response_down, 1)[0]
+
+    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton(text='А щас?', switch_inline_query_current_chat='status'))
+
+    result_id = f"{random.randint(1, 999)}_{is_up}"
+    result = types.InlineQueryResultArticle(id=result_id, title="Статус", input_message_content=types.InputTextMessageContent(status_message), reply_markup=keyboard)
+    await bot.answer_inline_query(inline_query.id, results=[result], cache_time=0)
+
 
 if __name__ == '__main__':
     try:
@@ -94,3 +111,4 @@ if __name__ == '__main__':
         executor.start_polling(dp, loop=loop, skip_updates=True)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
+
